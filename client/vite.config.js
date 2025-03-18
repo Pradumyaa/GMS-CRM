0e86da24936969ba973c80d5c178b0cadde5dc74
Pradumyaa <pradumya@getmaxsolutions.com>
2025-03-18 21:43:04 +0530

    Update vite.config.js - Added Server Property

--- client/vite.config.js
+++ client/vite.config.js
@@ -29,4 +29,8 @@ export default defineConfig({
   define: {
     global: {},
   },
+  server: {
+    host: '0.0.0.0',   // Bind to all network interfaces
+    port: process.env.PORT || 4173, // Use Render's PORT variable or fallback to 4173
+  }
 })
