cef7a66c32c1c10bc783cccef591a5ab63473559
Pradumyaa <pradumya@getmaxsolutions.com>
2025-03-18 21:38:06 +0530

    Update constants.js - Added backend path

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const HOST = import.meta.env.VITE_SERVER_URL;
+export const HOST = import.meta.env.VITE_BACKEND_URL;
 
 export const AUTH_ROUTES = "api/auth";
 // export const REGISTER_ROUTE = `${AUTH_ROUTES}/register`;
