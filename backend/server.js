05ad8673551b4343a9d64c26252fd1c73f21edf9
Pradumyaa <pradumya@getmaxsolutions.com>
2025-03-18 22:00:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -182,7 +182,7 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
